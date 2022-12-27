0a10d18610e92f571d8d4f1685ecced58343658a
mrityu <mkctriangle@yahoo.in>
2022-12-27 19:57:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import { AppContext } from "./utils/ContextApi";
+
+function App() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/" element={<SearchResult/>}></Route>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
+import { useNavigate, useParams } from "react-router-dom";
+
+import MicIcon from "../assets/mic.svg";
+import ImageIcon from "../assets/image.svg";
+
+const SearchInput = () => {
+    const [searchQuery,setSearchQuery]=useState("");
+    return (
+      <div id="searchBox" classname="h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c
+      focus-within:border-0 ">
+        <AiOutlineSearch size={18} color="#9aa0a6"/>
+        <input type="text" onChange={(e)=>console.log(e.target.value)} 
+        onKeyUp={(e)=>console.log(e.target.value)}
+        value={searchQuery} autoFocus
+        className="grow outline-0 text-black/[0.87]"
+        
+        ></input>
+      </div>
+    );
+};
+
+export default SearchInput;
